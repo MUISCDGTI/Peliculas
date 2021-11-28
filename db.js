c2ef924120d11ea97bfbbe52bd81a01c13570a7e
David Romero <davromorg@alum.us.es>
2021-11-28 20:12:40 +0100

    fix: Fix conflicts

--- db.js
+++ db.js
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-const mongoose = require("mongoose");
-const DB_URL = process.env.MONGO_URL || "mongodb://localhost:27017"; // dirección alternativa -> mongodb://db/test
-
-const dbConnect = function () {
-  const db = mongoose.connection;
-  db.on("error", console.error.bind(console, "connection error: "));
-  return mongoose.connect(DB_URL, { useNewUrlParser: true });
-};
-
-=======
 const mongoose = require('mongoose');
 const DB_URL = (process.env.MONGO_URL || 'mongodb://localhost:27017');
 
@@ -18,5 +7,4 @@ const dbConnect = function() {
     return mongoose.connect(DB_URL, { useNewUrlParser: true });
 }
 
->>>>>>> master
 module.exports = dbConnect;
--- index.js
+++ index.js
@@ -1,59 +1,3 @@
-<<<<<<< HEAD
-var express = require('express');
-var db = require("./db");
-var bodyParser = require('body-parser');
-const Film = require('./films');
-
-var port = (process.env.PORT || 3000); 
-var BASE_API_PATH = "/api/v1";
-
-console.log("Starting API server...");
-
-var app = express();
-app.use(bodyParser.json());
-
-db().then(
-  () => {
-    app.listen(port);
-    console.log("Server ready!");
-  },
-  (err) => {
-    console.log("Connection error: " + err);
-  }
-);
-
-app.get(BASE_API_PATH + "/", (req, res) => {
-  res.send("<html><body><h1>My server</h1></body></html>");
-});
-
-app.get(BASE_API_PATH + "/films", (req, res) => {
-  console.log(Date() + " - GET /films");
-
-  Film.find({}, (err, films) => {
-      if (err) {
-          console.log(Date() + "-" + err);
-          res.sendStatus(500);
-      } else {
-          res.send(films.map((film) => {
-              return film.all();
-          }));
-      }
-  });
-});
-app.post(BASE_API_PATH + "/films", (req, res) => {
-  console.log(Date() + " - POST /films");
-  var film = req.body;
-  Film.create(film, (err) => {
-      if (err) {
-          console.log(Date() + " - " + err);
-          res.sendStatus(500);
-      } else {
-          res.sendStatus(201);
-      }
-  });
-});
-module.exports = app;
-=======
 const app = require('./server.js');
 const dbConnect = require('./db');
 
@@ -70,4 +14,3 @@ dbConnect().then(
         console.log("Connection error: "+err);
     }
 )
->>>>>>> master
