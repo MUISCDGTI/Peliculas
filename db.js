d1ac03a3fb587a57350cf1edbd3ec0d4e82df7d3
David Romero <davromorg@alum.us.es>
2021-12-28 20:26:06 +0100

    fix: Delete MongoDB pod in Okteto

--- db.js
+++ db.js
@@ -1,5 +1,6 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
-const DB_URL = process.env.MONGO_URL || "mongodb://mongodb:27017";
+const DB_URL = process.env.MONGO_URL;
 
 const dbConnect = function() {
     const db = mongoose.connection;
